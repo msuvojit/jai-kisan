604483b4d93cc2e5a177471422729ff0586ceeb7
Suvojit Mondal <msuvojit@gmail.com>
2018-03-02 20:47:18 +0530

    add nodemon for autoreloading and fetch data from heroku server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    axios.get("https://shielded-headland-24739.herokuapp.com/static/users.txt")
+        .then(function (response) {
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
   res.render('index', { title: 'Express' });
 });
 
