ad2ffb7f72c899ef6153d261e97073fad6ee3c6c
Suvojit Mondal <msuvojit@gmail.com>
2018-03-03 00:32:13 +0530

    show users data in sortable table format

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,43 @@ var router = express.Router();
 var axios = require('axios');
 var _ = require('lodash');
 
-var usersCollection = [];
-
-function createUserObject(userString) {
-    var userObject = {};
-    const userArray = _.split(userString, '&');
-    _.forEach(userArray, function (dataString) {
-        const data = _.split(dataString, '=');
-        userObject[data[0]] = data[1];
-    });
-    // console.log(userObject);
-    usersCollection.push(userObject);
-    // console.log(usersCollection);
-}
+// var usersCollection = [];
+//
+// function createUserObject(userString) {
+//     var userObject = {};
+//     const userArray = _.split(userString, '&');
+//     _.forEach(userArray, function (dataString) {
+//         const data = _.split(dataString, '=');
+//         userObject[data[0]] = data[1];
+//     });
+//     usersCollection.push(userObject);
+// }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var usersCollection = [];
     axios.get("https://shielded-headland-24739.herokuapp.com/static/users.txt")
         .then(function (response) {
             const users = _.split(response.data, '|');
             _.forEach(users, function (user) {
-                createUserObject(user);
+                var userObject = {};
+                const userArray = _.split(user, '&');
+                _.forEach(userArray, function (dataString) {
+                    const data = _.split(dataString, '=');
+                    userObject[data[0]] = data[1];
+                });
+                usersCollection.push(userObject);
             });
-            console.log(usersCollection);
+            // console.log(usersCollection);
+            res.render('index', { title: 'Users List', users:  JSON.stringify(usersCollection) });
         })
         .catch(function (error) {
             console.log(error);
+            res.render('index', { title: 'Users List', users:  JSON.stringify(usersCollection) });
         });
 
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Users List', users:  JSON.stringify(usersCollection) });
+  // usersCollection = [];
 });
 
 module.exports = router;
