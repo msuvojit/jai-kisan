aac0fab1006d2cae3da9462b243d54e4c701531b
Suvojit Mondal <msuvojit@gmail.com>
2018-03-02 21:24:36 +0530

    process users data and store in a array in json format

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var _ = require('lodash');
+
+var usersCollection = [];
+
+function createUserObject(userString) {
+    var userObject = {};
+    const userArray = _.split(userString, '&');
+    _.forEach(userArray, function (dataString) {
+        const data = _.split(dataString, '=');
+        userObject[data[0]] = data[1];
+    });
+    // console.log(userObject);
+    usersCollection.push(userObject);
+    // console.log(usersCollection);
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     axios.get("https://shielded-headland-24739.herokuapp.com/static/users.txt")
         .then(function (response) {
-            console.log(response.data)
+            const users = _.split(response.data, '|');
+            _.forEach(users, function (user) {
+                createUserObject(user);
+            });
+            console.log(usersCollection);
         })
         .catch(function (error) {
             console.log(error);
         });
+
   res.render('index', { title: 'Express' });
 });
 
